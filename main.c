94edebce66a0aed69cbeb232f6f2de95d8f1ea80
ogoremeni <yacc@tuta.io>
2019-12-26 19:17:08 +0200

    fix: noise race condition

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "stdlib.h"
-#include "cy_pdl.h"
+ #include "cy_pdl.h"
 #include "cyhal.h"
 #include "cybsp.h"
 #include "cy_retarget_io.h"
@@ -7,21 +7,20 @@
 #include "cycfg.h"
 #include "cy_sysint.h"
 
-#include "sd_io.h"
-
 #define DEBUG 0
+#define ADC_DEBUG 0
 
 #define PDM_PCM_FIFO_TRG_LVL        128u
 // How many blocks to fetch in one go
-#define BLOCK_BATCH_NUM             1
+#define BLOCK_BATCH_NUM             2
 // How many of 16bit to store in one ping pong buffer
-#define BUFFER_SIZE                 BLOCK_BATCH_NUM * (CY_SD_HOST_BLOCK_SIZE / 2)
+#define BUFFER_SIZE                 512*512
 // ping pong buffer count
 #define BUFFERS_NUM	                2u
 #define THRESHOLD_HYSTERESIS        4u
 #define VOLUME_RATIO                (1024u)
 
-uint16_t buffers[BUFFERS_NUM][BUFFER_SIZE];
+uint16_t buffer[BUFFER_SIZE];
 
 volatile bool pdm_pcm_flag = false;
 volatile bool record_mode = false;
@@ -35,9 +34,7 @@ volatile uint8_t sineidx = 0;
 
 static const uint16_t sinetable[256] = {31, 32, 33, 33, 34, 35, 36, 36, 37, 38, 39, 39, 40, 41, 41, 42, 43, 43, 44, 45, 45, 46, 46, 47, 48, 48, 49, 49, 50, 50, 51, 51, 51, 52, 52, 53, 53, 53, 54, 54, 54, 55, 55, 55, 55, 56, 56, 56, 56, 56, 56, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 58, 58, 58, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 56, 56, 56, 56, 56, 56, 55, 55, 55, 55, 54, 54, 54, 53, 53, 53, 52, 52, 51, 51, 51, 50, 50, 49, 49, 48, 48, 47, 46, 46, 45, 45, 44, 43, 43, 42, 41, 41, 40, 39, 39, 38, 37, 36, 36, 35, 34, 33, 33, 32, 31, 30, 29, 29, 28, 27, 26, 26, 25, 24, 23, 23, 22, 21, 21, 20, 19, 19, 18, 17, 17, 16, 16, 15, 14, 14, 13, 13, 12, 12, 11, 11, 11, 10, 10, 9, 9, 9, 8, 8, 8, 7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 11, 11, 11, 12, 12, 13, 13, 14, 14, 15, 16, 16, 17, 17, 18, 19, 19, 20, 21, 21, 22, 23, 23, 24, 25, 26, 26, 27, 28, 29, 29, 30};
 
-uint8_t read_buffer_idx = 0;
-uint8_t write_buffer_idx = 0;
-uint16_t write_idx = 127;
+uint16_t write_idx = 0;
 uint16_t read_idx = 0;
 
 const cy_stc_sysint_t pdm_pcm_isr_cfg = {
@@ -61,42 +58,23 @@ const cy_stc_sysint_t fetcher_isr_cfg = {
 
 uint32_t chan = 0UL;
 uint32_t intr_status = 0u;
-
-cy_stc_sd_host_write_read_config_t sdcard_data = {
-    .numberOfBlocks = BLOCK_BATCH_NUM,
-    .autoCommand = CY_SD_HOST_AUTO_CMD_NONE,
-    .dataTimeout = 12UL,
-    .enReliableWrite = false,
-    .enableDma = true,
-    .data = buffers[0],
-    .address = 0u
-};
+uint16_t res = 0;
+uint16_t adc_code = 0;
 
 void SAR_Interrupt() {
     intr_status = Cy_SAR_GetInterruptStatus(SAR);
 
     if ((intr_status & (uint32_t) CY_SAR_INTR_EOS_MASK) == (uint32_t) CY_SAR_INTR_EOS_MASK) {
-        buffers[write_buffer_idx][write_idx++] = Cy_SAR_GetResult16(SAR, chan);
+        adc_code = Cy_SAR_GetResult16(SAR, chan) >> 6;
 
-        if (DEBUG)
-            printf("(adc): %d\r\n", buffers[write_buffer_idx][write_idx]);
+        Cy_TCPWM_PWM_SetCompare0(PLAYER_HW, PLAYER_NUM, adc_code);
 
-        if (write_idx == BUFFER_SIZE) {
-
-            // Time to save the buffer
-            if (record_mode) {
-                sdcard_data.data = (uint32_t*) buffers[write_buffer_idx];
-                sdcard_data.address += BUFFER_SIZE;
-
-                Cy_SD_Host_Write(SDHC1, &sdcard_data, &sdHostContext);
-            }
-
-            write_idx = 0;
-            write_buffer_idx += 1;
-
-            if (write_buffer_idx == BUFFERS_NUM)
-                write_buffer_idx = 0;
+        if (record_mode) {
+            buffer[write_idx++] = adc_code;
         }
+
+        if (ADC_DEBUG)
+            printf("(adc): %d\r\n", adc_code);
     }
 
     Cy_SAR_ClearInterrupt(SAR, intr_status);
@@ -120,21 +98,18 @@ void pdm_pcm_isr_handler() {
 }
 
 void sine_isr_handler() {
-    Cy_TCPWM_PWM_SetCompare0(ANOTHER_HW, ANOTHER_NUM, sinetable[sineidx++]);
-    Cy_TCPWM_ClearInterrupt(SINE_GIVER_HW, SINE_GIVER_NUM, CY_TCPWM_INT_ON_TC);
+    Cy_TCPWM_PWM_SetCompare0(SINE_PLAYER_HW, SINE_PLAYER_NUM, sinetable[sineidx++]);
+    Cy_TCPWM_ClearInterrupt(SINE_FETCHER_HW, SINE_FETCHER_NUM, CY_TCPWM_INT_ON_TC);
 }
 
 void fetcher_isr_handler() {
-    Cy_TCPWM_PWM_SetCompare0(PLAYER_HW, PLAYER_NUM, buffers[read_buffer_idx][read_idx++]);
     Cy_TCPWM_ClearInterrupt(FETCHER_HW, FETCHER_NUM, CY_TCPWM_INT_ON_TC);
 
-    if (read_idx >= BUFFER_SIZE) {
-        read_buffer_idx += 1;
+    if (!record_mode) {
+        Cy_TCPWM_PWM_SetCompare0(PLAYER_HW, PLAYER_NUM, buffer[read_idx++]);
 
-        if (read_buffer_idx >= BUFFERS_NUM)
-            read_buffer_idx = 0;
-
-        read_idx = 0;
+        if (read_idx >= BUFFER_SIZE)
+            read_idx = 0;
     }
 }
 
@@ -174,27 +149,21 @@ int main() {
 
     Cy_GPIO_Pin_FastInit(CYBSP_PIN12_PORT, CYBSP_PIN12_NUM, CY_GPIO_DM_STRONG, 1, HSIOM_SEL_GPIO);
 
-    Cy_SysInt_Init(&pdm_pcm_isr_cfg, pdm_pcm_isr_handler);
-    /* NVIC_EnableIRQ(pdm_pcm_isr_cfg.intrSrc); */
-    Cy_PDM_PCM_Init(CYBSP_PDM_PCM_HW, &CYBSP_PDM_PCM_config);
-    Cy_PDM_PCM_ClearFifo(CYBSP_PDM_PCM_HW);
-    Cy_PDM_PCM_Enable(CYBSP_PDM_PCM_HW);
-
     // 6-bit left pwm
     Cy_TCPWM_PWM_Init(PLAYER_HW, PLAYER_NUM, &PLAYER_config);
     Cy_TCPWM_PWM_Enable(PLAYER_HW, PLAYER_NUM);
     Cy_TCPWM_TriggerStart(PLAYER_HW, PLAYER_MASK);
 
-    Cy_TCPWM_PWM_Init(ANOTHER_HW, ANOTHER_NUM, &ANOTHER_config);
-    Cy_TCPWM_PWM_Enable(ANOTHER_HW, ANOTHER_NUM);
-    Cy_TCPWM_TriggerStart(ANOTHER_HW, ANOTHER_MASK);
+    Cy_TCPWM_PWM_Init(SINE_PLAYER_HW, SINE_PLAYER_NUM, &SINE_PLAYER_config);
+    Cy_TCPWM_PWM_Enable(SINE_PLAYER_HW, SINE_PLAYER_NUM);
+    Cy_TCPWM_TriggerStart(SINE_PLAYER_HW, SINE_PLAYER_MASK);
 
     /* Fetching 440Hz test sine */
     Cy_SysInt_Init(&sine_isr_cfg, sine_isr_handler);
     NVIC_EnableIRQ(sine_isr_cfg.intrSrc);
-    Cy_TCPWM_PWM_Init(SINE_GIVER_HW, SINE_GIVER_NUM, &SINE_GIVER_config);
-    Cy_TCPWM_TriggerStart(SINE_GIVER_HW, SINE_GIVER_MASK);
-    Cy_TCPWM_PWM_Enable(SINE_GIVER_HW, SINE_GIVER_NUM);
+    Cy_TCPWM_PWM_Init(SINE_FETCHER_HW, SINE_FETCHER_NUM, &SINE_FETCHER_config);
+    Cy_TCPWM_TriggerStart(SINE_FETCHER_HW, SINE_FETCHER_MASK);
+    Cy_TCPWM_PWM_Enable(SINE_FETCHER_HW, SINE_FETCHER_NUM);
 
     // fetching real data
     Cy_SysInt_Init(&fetcher_isr_cfg, fetcher_isr_handler);
@@ -205,14 +174,14 @@ int main() {
 
     adc_init();
 
-    initCard();
-
     printf("CY_SD_HOST_BLOCK_SIZE: %d\r\n", CY_SD_HOST_BLOCK_SIZE);
     printf("BUFFER_SIZE: %d\r\n", BUFFER_SIZE);
 
     printf("(sdcard) is online\r\n");
 
-    sdcard_test();
+    for (uint16_t idx = 0; idx < BUFFER_SIZE; idx++) {
+        buffer[idx] = 0;
+    }
 
     for(;;);
 }
